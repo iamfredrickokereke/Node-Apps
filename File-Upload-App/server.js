b6b616f335974426ef9d9ccc3f91b60850971bf0
Kelly online <kellyonline2016@gmail.com>
2020-04-15 11:21:33 +0100

    added a on event emmitter

--- File-Upload-App/server.js
+++ File-Upload-App/server.js
@@ -2,3 +2,10 @@ const http = require('http');
 
 const server = http.createServer();
 
+server.on('request', (request, response) => {
+    console.log("incoming request...");
+    
+})
+
+
+server.listen(8000);
