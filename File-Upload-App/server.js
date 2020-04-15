fec5695e2dc0877e1d50edf7f1db4c840f6dda9f
Kelly online <kellyonline2016@gmail.com>
2020-04-15 13:12:49 +0100

    updates

--- File-Upload-App/server.js
+++ File-Upload-App/server.js
@@ -18,7 +18,7 @@ server.on('request', (request, response) => {
     // console.log(`The request method is: "${request.method}", and the url is "${request.url}"`);
 
     // checking the request.url method  using the url module
-    console.log("hey");
+    console.log(request.headers);
     
     const parsedUrl = url.parse(request.url, true); // for query string
 
@@ -28,15 +28,24 @@ server.on('request', (request, response) => {
         const { id } = parsedUrl.query;
         console.log(id);
         const metadata = services.fetchImageMetadata(id);
-        console.log(metadata);
-        
-        
+        console.log(metadata);        
+    }else{
+
+        // when sending a response to a user we must have 3 things
+        response.statusCode = 404;
+        response.setHeader('X-Powered-By', 'Fred');
+        response.end("bye")  //close connection
     }
 
-    
+    // const body = [];
+    // request.on('data', (chunk) => {
+    //     body.push(chunk);
+    // }).on('end', () => {
+    //     const parsedJSON = JSON.parse(Buffer.concat(body));
+    //     console.log(parsedJSON);
+        
+    // })
 
-    
-    
 })
 
 //assign and listen to port
