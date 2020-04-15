372d81b640c95f17a5caca0ef04cbf68aa170417
Kelly online <kellyonline2016@gmail.com>
2020-04-15 11:37:54 +0100

    output request objects and fixed port number

--- File-Upload-App/server.js
+++ File-Upload-App/server.js
@@ -2,11 +2,14 @@ const http = require('http');
 
 const server = http.createServer();
 
-const port = 8080;
+const port = 8000;
 
 // when server is on request
 server.on('request', (request, response) => {
     console.log(" this is an incoming request...");
+
+    console.table(request.method, request.url);
+    
     
 })
 
