eb3aa9aef63c8d2935c196d91b4eaa04b14abee7
Kelly online <kellyonline2016@gmail.com>
2020-05-17 11:11:42 +0100

    set statuscode and pass json data

--- Basic-server/server.js
+++ Basic-server/server.js
@@ -2,11 +2,19 @@
 const http = require('http');
 
 
-// call the create server method
+// call the create server method and set headers via response call back
 const server = http.createServer( (request, response) => {
 
-    response.setHeader('Content-Type', 'application/json');
-    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Content-Type', 'application/json'); //object-type
+    response.setHeader('Access-Control-Allow-Origin', '*'); //cors
+    response.writeHead(200); //status code - OK
+
+    let dataObject = {"id": 1234, "name":"kelly", "sex":"male"};
+
+    let data = JSON.stringify(dataObject);
+
+
+    response.end(data);
 });
 
 //listen on a port and send a message to console
