62235357da57f77efd0215f35ff23d3732802e5b
Kelly online <kellyonline2016@gmail.com>
2020-05-17 10:58:12 +0100

    set headers

--- Basic-server/server.js
+++ Basic-server/server.js
@@ -3,7 +3,11 @@ const http = require('http');
 
 
 // call the create server method
-const server = http.createServer();
+const server = http.createServer( (request, response) => {
+
+    response.setHeader('Content-Type', 'application/json');
+    response.setHeader('Access-Control-Allow-Origin', '*');
+});
 
 //listen on a port and send a message to console
 server.listen(1234, () => { console.log(`Server is listening on port ${1234}`);
