7c8d411f53dc78d1de4be3bd35e388a8bdbabe46
Kelly online <kellyonline2016@gmail.com>
2020-05-18 23:19:03 +0100

    modified app file and added packages

--- Scripts/Debug/app.js
+++ Scripts/Debug/app.js
@@ -5,10 +5,10 @@ var name = 'fred';
 var age = 32;
 
 
-console.log(name + '...from console.log');
-console.log(age + '...from console.log');
+console.log('my name is' + name + '...from console.log');
+console.log('my age is' + age + '...from console.log');
 
-debug(name + '...from dubug')
-debug(age + '...from debug')
+debug('my name is' + name + '...from dubug')
+debug('my age is' + age + '...from debug')
 
 debug('this stuff works man')
